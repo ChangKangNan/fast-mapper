df2a7fbcfe739c861d13993860c4b8dfae4fb193
kangnan.chang <ckn335019175@163.com>
2022-11-02 14:16:26 +0800

    删除多余jar

--- src/main/java/cn/ft/ckn/fastmapper/util/TransactionSwitch.java
+++
@@ -1,7 +0,0 @@
-package cn.ft.ckn.fastmapper.util;
-
-import io.netty.util.concurrent.FastThreadLocal;
-
-public class TransactionSwitch {
-    public static FastThreadLocal<Boolean> GLOBAL_TRANSACTION_SWITCH_STATUS=new FastThreadLocal<>();
-}
